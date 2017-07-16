78844e6cb01db23fe21b1781bb01b2dd290cbe70
David Carrera del Castillo <eoldavix@gmail.com>
2017-07-16 12:40:16 +0200

    Sincronizado con últiima versión estable

--- siskobet/siskobet.py
+++ siskobet/siskobet.py
@@ -13,7 +13,18 @@ except ImportError:
 
 def html_to_text(texto):
     """ De HTML a texto """
-    return BeautifulSoup(texto).body.get_text().split('\n')
+
+    salida = ""
+
+    if "datos-apuesta" in texto:
+        for line in texto.split('\n'):
+            matching = re.match(r"^.*(http.*datos-apuesta[\S]*).*$", line)
+            if matching:
+                salida += matching.group(1) + "\n"
+
+        return salida.split('\n') or u''
+    else:
+        return BeautifulSoup(texto, "lxml").body.get_text().split('\n')
 
 
 def parsea_texto(texto):
@@ -24,9 +35,8 @@ def parsea_texto(texto):
     regexes = [r"^[*]*[\s]*[0-9]+[.:][0-9]+",
                r"[pP][tT][sS]* [eE][/]*[wW]",
                r"[pP][tT][sS]* [wW][iI][nN]",
-               r"datos-apuesta",
                r"[0-9]+ [pP][oO][iI][nN][tT][sS]*",
-               r"^Tip",
+               r"^Tip #",
               ]
 
     combined = "(" + ")|(".join(regexes) + ")"
@@ -35,7 +45,8 @@ def parsea_texto(texto):
         for line in texto:
             if re.search(combined, line) \
                or "points per line" in line.lower() \
-               or "No selections today" in line:
+               or "No selections today" in line \
+               or "datos-apuesta" in line:
 
                 if "Recommendation" in line:
                     matching = re.match(".*(Recommendation.*)", line)
@@ -54,13 +65,14 @@ def parsea_texto(texto):
         pass
 
 def main():
-    texto = sys.stdin.read()
+    texto = unicode(sys.stdin.read(), errors='ignore')
 
     # for link in html.body.find_all('a'):
     #     if datos-apuesta in link:
     #         print(link.get('href'))
 
     print parsea_texto(html_to_text(texto))
+    #html_to_text(texto)
 
 if __name__ == '__main__':
     main()
